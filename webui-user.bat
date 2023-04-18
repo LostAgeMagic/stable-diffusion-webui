d15e1ffbf3a8415312000a939592ccbdbb087088
Exile <63306347+LostAgeMagic@users.noreply.github.com>
2023-04-17 19:23:20 -0500

    Internal Update

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,7 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set COMMANDLINE_ARGS=--ckpt-dir "E:\WebUIBackup\models\Stable-diffusion" --xformers --medvram --skip-version-check --precision full --no-half --no-half-vae --disable-nan-check --force-enable-xformers --opt-split-attention --always-batch-cond-uncond --autolaunch
+set PYTORCH_CUDA_ALLOC_CONF=garbage_collection_threshold:0.9,max_split_size_mb:512
 
 call webui.bat
